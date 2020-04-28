262614ac03665ff1cbbda65931befe7c757ee3a7
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-28 09:55:09 -0300

    criando rotas iniciais

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Lucas' }),
-);
+// Toda rota que iniciar com /appointments, vou é necessário re-escrever
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
