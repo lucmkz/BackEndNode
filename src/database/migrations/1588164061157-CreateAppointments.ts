19f9c0b1236db543fcffdfe1d198782fb509aa1d
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-30 10:30:32 -0300

    relationship tables

--- src/database/migrations/1588164061157-CreateAppointments.ts
+++ src/database/migrations/1588164061157-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588164061157
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1588196993767-CreateUser.ts
+++ src/database/migrations/1588196993767-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1588196993767 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
