0b6713d59283eb17a3b671cfde3244d904c0e521
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-30 11:06:01 -0300

    create user adn refactroring

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 // Toda rota que iniciar com /appointments, vou é necessário re-escrever
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
