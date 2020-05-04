08a263da2faf87f0fb7df07d8d4410a251eee31e
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-04 10:49:48 -0300

    create session JWT

---
+++ src/Services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+class AuthentiateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect Email/Password combination');
+    }
+
+    // user.password senha criptografada
+    // passwors - Senha não criptografada
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect Email/Password combination');
+    }
+
+    // usuário autenticado
+
+    return {
+      user,
+    };
+  }
+}
+
+export default AuthentiateUserService;
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import AuthentiateUserService from '../Services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthentiateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
