328fe292f37cbd47c671a38050619a07bda4b043
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-04 16:21:12 -0300

    global handling error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 
 import uploadConfig from './config/upload';
+
+import AppError from './errors/AppErros';
 
 import './database';
 
@@ -11,6 +14,22 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 const port = 3333;
 
