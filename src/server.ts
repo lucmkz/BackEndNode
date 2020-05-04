b1bd98721c40e495f4a4bd832b10ab8985336333
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-04 15:45:37 -0300

    serving static folder

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 const port = 3333;
