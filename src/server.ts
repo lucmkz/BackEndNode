43ab2c867bca719f9bfc7857ec919c09652891dd
Lucas Duarte <l.duarte.mk@gmail.com>
2020-06-22 10:45:19 -0300

    alteraçoès backend

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`Server started at Port ${port}`);
